b680478a06ceadc2c1504588f6498b7e1ffa642b
theskethtl <travis.hesketh@strath.ac.uk>
2019-06-21 13:11:46 +0100

    Moved to subdirectory

--- slurm_template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --export=ALL
-#SBATCH --output=PEPTIDE_slurm.log
-#SBATCH --job-name=PEPTIDE_eq_coarse
-module purge
-module load ${GROMACS_VERSION}
-mpirun -np ${SLURM_NTASKS} gmx_mpi mdrun -ntomp 1 -deffnm PEPTIDE_eq
