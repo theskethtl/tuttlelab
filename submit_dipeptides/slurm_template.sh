2dbfa81410a45af0d26d5a7f35205245196e4c79
Travis Hesketh <travis.hesketh@strath.ac.uk>
2019-06-24 11:43:32 +0100

    Updated versions of these files.

--- submit_dipeptides/slurm_template.sh
+++ submit_dipeptides/slurm_template.sh
@@ -3,5 +3,5 @@
 #SBATCH --output=PEPTIDE_slurm.log
 #SBATCH --job-name=PEPTIDE_eq_coarse
 module purge
-module load ${GROMACS_VERSION}
+module load ${GROMACS_MODULE}
 mpirun -np ${SLURM_NTASKS} gmx_mpi mdrun -ntomp 1 -deffnm PEPTIDE_eq
